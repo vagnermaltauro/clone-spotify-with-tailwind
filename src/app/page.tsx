02d9aa202fe754fcedea998d163012fe2a902175
vagnermaltauro <vagnermaltauro@gmail.com>
2023-05-06 18:09:31 -0300

    feat(page.tsx): add playlist navigation to sidebar to improve user experience and allow easy access to playlists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,13 @@ export default function Home() {
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
-          <nav className="space-y-5">
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full" />
+            <div className="w-3 h-3 bg-yellow-500 rounded-full" />
+            <div className="w-3 h-3 bg-green-500 rounded-full" />
+          </div>
+
+          <nav className="space-y-5 mt-10">
             <a href="/" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
               <HomeIcon />
               Home
@@ -21,6 +27,13 @@ export default function Home() {
               <Library />
               Your Library
             </a>
+          </nav>
+
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 1</a>
+            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 2</a>
+            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 3</a>
+            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 4</a>
           </nav>
         </aside>
 
