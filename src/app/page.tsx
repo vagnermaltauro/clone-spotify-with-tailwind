a7251fea0587455fb71e4018675e9ae26c0bb67d
vagnermaltauro <vagnermaltauro@gmail.com>
2023-05-06 22:33:26 -0300

    refactor(page.tsx): extract sidebar and footer to separate components to improve code organization and reusability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {
-  Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight,
-} from 'lucide-react';
 import AlbumButton from 'components/AlbumButton';
 import MadeForUser from 'components/MadeForUser';
+import Footer from 'components/Footer';
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react';
+import Sidebar from 'components/Sidebar';
 
 const albumImages = [
   {
@@ -42,37 +42,7 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-6">
-          <div className="flex items-center gap-2">
-            <div className="w-3 h-3 bg-red-500 rounded-full" />
-            <div className="w-3 h-3 bg-yellow-500 rounded-full" />
-            <div className="w-3 h-3 bg-green-500 rounded-full" />
-          </div>
-
-          <nav className="space-y-5 mt-10">
-            <a href="/" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-              <HomeIcon />
-              Home
-            </a>
-
-            <a href="/" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-              <Search />
-              Search
-            </a>
-
-            <a href="/" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-              <Library />
-              Your Library
-            </a>
-          </nav>
-
-          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
-            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 1</a>
-            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 2</a>
-            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 3</a>
-            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 4</a>
-          </nav>
-        </aside>
+        <Sidebar />
 
         <main className="flex-1 p-6">
           <div className="flex items-center gap-3">
@@ -101,9 +71,7 @@ export default function Home() {
         </main>
       </div>
 
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
-      </footer>
+      <Footer />
     </div>
   );
 }
